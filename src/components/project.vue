4b24ff904b53dafd321fbdb93f280477f1da5713
ajaykrupalk <ajaykrupalk@outlook.in>
2023-04-01 19:18:49 +0530

    Refactoring the code

---
+++ src/components/project.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    :class="`
+      group
+      relative
+      flex flex-col
+      w-full
+      bg-gray-800
+      h-60
+      text-white
+      rounded-md
+      p-5
+      hover:bg-amber-600
+      ${size}`
+    "
+  >
+    <a
+      :href="`${link}`"
+      class="absolute top-0 left-0 z-10 w-full h-full"
+      target="_blank"
+    ></a>
+    <div class="group-hover:hidden">
+      <h2 class="text-lg font-bold md:text-xl">{{ title }}</h2>
+      <p class="text-md">{{ description }}</p>
+    </div>
+    <div :class="`${imgposition} p-3 absolute bottom-0`">
+      <img
+        :src="`${imgsrc}`"
+        alt=""
+        :class="`${imgsize} group-hover:scale-125 group-hover:-translate-y-3 transition ease-in duration-300`"
+      />
+    </div>
+    <div class="group-hover:block hidden">
+      <div class="flex flex-col">
+        <div>
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Project",
+    props: {
+        size: String,
+        link: String,
+        title: String,
+        description: String,
+        imgposition: String,
+        imgsrc: String,
+        imgsize: String,
+    }
+}
+</script>
--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,45 +1,134 @@
 <template>
     <div class="flex flex-wrap justify-center gap-2 md:m-5 md:w-10/12 lg:8/12">
         <!-- About -->
-        <About />
+        <Card attr="relative flex flex-col lg:w-6/12 pt-16 md:leading-10" >
+            <template v-slot:content>
+                <h1 class="text-lg font-bold md:text-4xl">
+                        Hey, I'm Ajay <span class="wave">👋🏼</span>
+                </h1>
+                <p class="text-md font-medium">
+                    "Unleashing creativity, one line of code at a time."
+                </p>
+            </template>
+            <template v-slot:onhover>
+                <p class="text-lg font-medium">Contact at:</p>
+                <h3 class="text-lg font-bold md:text-4xl">
+                    <a href="mailto:ajaykrupalk@gmail.com">ajaykrupalk@gmail.com</a>
+                </h3>
+            </template>
+        </Card>
+
         <!-- Achievements -->
-        <Achievements />
+        <Card attr="pt-10 lg:w-5/12">
+            <template v-slot:nohover>
+                <h2 class="text-lg font-bold md:text-3xl">Achievements</h2>
+                <div class="p-3">
+                    <ul class="list-disc">
+                        <li>Self taught Web Developer</li>
+                        <li>AWS Certified Cloud Practitioner</li>
+                        <li>Former Tech Intern at IIM-Bangalore</li>
+                    </ul>
+                </div>
+            </template>
+        </Card>
+
         <!-- Markdown -->
-        <Markdown />
+        <Project 
+            size="lg:w-3/12"
+            link="https://git-react-markdown.netlify.app/"
+            title="Markdown App"
+            description="Built using ReactJS and TailwindCSS"
+            imgposition="self-end"
+            imgsrc="/src/assets/ph-person.png"
+            imgsize="h-32 w-12"
+        >
+            <h2 class="text-lg font-bold md:text-xl">
+                View<br/>The<br />App<br/>
+            </h2>
+        </Project>
+        
         <!-- CodePen Clone -->
-        <CodePen />
+        <Project 
+            size="lg:w-4/12"
+            link="https://github.com/ajaykrupalk/codepen-clone"
+            title="CodePen Clone"
+            description="Built using ReactJS"
+            imgsize="h-36 w-26"
+            imgposition="self-end"
+            imgsrc="/src/assets/comp-person.png"
+        >
+            <h2 class="text-lg font-bold md:text-xl">
+                View <br />Source<br />Code
+            </h2>
+        </Project>
+        
         <!-- Movie App -->
-        <Movie />
+        <Project 
+            size="lg:w-4/12"
+            link="https://moviereactland.netlify.app/"
+            title="Movie Finder Website"
+            description="Built using ReactJS and OMDb API"
+            imgsize="h-20 w-26"
+            imgposition="self-start"
+            imgsrc="/src/assets/laying-person.png"
+        >
+            <h2 class="text-lg font-bold md:text-xl">
+                View The App
+            </h2>
+        </Project>
+        
         <!-- Weather App -->
-        <Weather />
+        <Project 
+            size="lg:w-4/12"
+            link="https://github.com/ajaykrupalk/Weather-App"
+            title="Weather App"
+            description="Built using VueJS, Laravel and TailwindCSS"
+            imgsize="h-36 w-26"
+            imgposition="self-end"
+            imgsrc="/src/assets/study-person.png"
+        >
+            <h2 class="text-lg font-bold md:text-xl">View <br> Source<br>Code</h2>
+        </Project>
+        
+
         <!-- NBA Bot -->
-        <Bot />
+        <Project 
+            size="lg:w-3/12"
+            link="https://github.com/ajaykrupalk/NBA-Bot"
+            title="Discord NBA Bot"
+            description="Built using Python with Discord.py"
+            imgsize="h-28 w-26"
+            imgposition="self-center"
+            imgsrc="/src/assets/jump-person.png"
+        >
+            <h2 class="text-lg font-bold md:text-xl">View <br> Source<br>Code</h2>
+        </Project>
+        
+
         <!-- TechTools -->
-        <TechTools />
+        <Project 
+            size="lg:w-4/12"
+            link="https://github.com/ajaykrupalk/TechTools"
+            title="TechTools"
+            description="Built using VueJS, Laravel and TailwindCSS"
+            imgsize="h-28 w-26"
+            imgposition="self-center"
+            imgsrc="/src/assets/casual-person.png"
+        >
+            <h2 class="text-lg font-bold md:text-xl">View <br> Source<br>Code</h2>
+        </Project>
     </div>
 </template>
 
 <script>
-import About from './about.vue';
-import Achievements from './achievements.vue' ;
-import Markdown from './markdown.vue';
-import CodePen from './codepen.vue';
-import Movie from './movie.vue';
-import Weather from './weather.vue';
-import Bot from './bot.vue';
-import TechTools from './techtools.vue';
+import Card from './card.vue';
+import Project from './project.vue';
 
 export default {
     name: 'Projects',
     components: {
-        About,
-        Achievements,
-        Markdown,
-        CodePen,
-        Movie,
-        Weather,
-        Bot,
-        TechTools
+        Card,
+        Project,
     }
     
 }
