3c97a0c37a13a953f6292e0c192062fa1f691941
ajaykrupalk <ajaykrupalk@outlook.in>
2023-04-01 19:43:50 +0530

    Refactoring image path

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,34 +1,34 @@
-<template>
-<!-- pass attr as prop to the class of a div -->
-    <div
-        :class="`
-        group
-        w-full
-        bg-gray-800
-        h-60
-        text-white
-        rounded-md
-        p-5
-        hover:bg-amber-600
-        ${attr}`"
-    >   
-        <slot name="nohover"></slot>
-        <div class="group-hover:hidden">
-            <slot name="content"></slot>
+    <template>
+    <!-- pass attr as prop to the class of a div -->
+        <div
+            :class="`
+            group
+            w-full
+            bg-gray-800
+            h-60
+            text-white
+            rounded-md
+            p-5
+            hover:bg-amber-600
+            ${attr}`"
+        >   
+            <slot name="nohover"></slot>
+            <div class="group-hover:hidden">
+                <slot name="content"></slot>
+            </div>
+            <slot name="image"></slot>
+            <div class="group-hover:block hidden">
+                <slot name="onhover"></slot>
+            </div>
         </div>
-        <slot name="image"></slot>
-        <div class="group-hover:block hidden">
-            <slot name="onhover"></slot>
-        </div>
-    </div>
-</template>
+    </template>
 
-<script>
-export default {
-  name: "Card",
-  props:{
-    attr: String,
-  },
-}
+    <script>
+    export default {
+    name: "Card",
+    props:{
+        attr: String,
+    },
+    }
 
-</script>
+    </script>
