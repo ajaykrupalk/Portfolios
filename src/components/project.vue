7a916209be16814e7ac6f5f2698f32ba301c1a22
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2023-04-01 20:14:21 +0530

    test path

--- src/components/project.vue
+++ src/components/project.vue
@@ -25,7 +25,7 @@
     </div>
     <div :class="`${imgposition} p-3 absolute bottom-0`">
       <img
-        src="../assets/ph-person.png"
+        :src="imgsrc"
         alt=""
         :class="`${imgsize} group-hover:scale-125 group-hover:-translate-y-3 transition ease-in duration-300`"
       />
