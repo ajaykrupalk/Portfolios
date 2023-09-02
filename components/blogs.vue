cc9cbc9f65faf08eb640f55d2d58acff21f5dd7c
ajaykrupalk <ajaykrupalk@outlook.in>
2023-09-02 21:07:05 +0530

    Changed blog names

--- components/blogs.vue
+++ components/blogs.vue
@@ -4,16 +4,15 @@
             <h3 class="font-mono text-xl">blogs</h3>
         </div>
         <div class="flex justify-between">
-            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">the race for building ai</h4>
+            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">vercel ai: an oss sdk</h4>
             <p class="text-sm text-gray-300/75">aug 31, 2023</p>
         </div>
         <div class="flex justify-between">
-            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">why oss is important?</h4>
+            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">the hype behind llm's</h4>
             <p class="text-sm text-gray-300/75">aug 15, 2023</p>
         </div>
         <div class="flex justify-between">
-            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">how chatgpt has improved over the
-                months</h4>
+            <h4 class="text-base font-medium underline underline-offset-4 decoration-1">why nuxt over vue?</h4>
             <p class="text-sm text-gray-300/75">aug 01, 2023</p>
         </div>
         <div>
