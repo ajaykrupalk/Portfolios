9ed7d5bfbaf79715132c572ebb828ceaf8d32229
ajaykrupalk <ajaykrupalk@outlook.in>
2023-09-02 22:33:37 +0530

    changed section names

--- components/blogs.vue
+++ components/blogs.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="space-y-4">
         <div>
-            <h3 class="font-mono text-xl">blogs</h3>
+            <h3 class="font-mono text-xl">blog</h3>
         </div>
         <div class="flex justify-between">
             <h4 class="text-base font-medium underline underline-offset-4 decoration-1">vercel ai: an oss sdk</h4>
--- components/playground.vue
+++ components/playground.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="space-y-4">
         <div>
-            <h3 class="font-mono text-xl">playground</h3>
+            <h3 class="font-mono text-xl">projects</h3>
         </div>
         <div>
             <h4 class="text-base font-medium underline underline-offset-4 decoration-1">shadcn/ui</h4>
