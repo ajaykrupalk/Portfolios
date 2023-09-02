f420389988b2be5aa992b7d73521f1ef5559d4f5
ajaykrupalk <ajaykrupalk@outlook.in>
2023-09-02 21:02:36 +0530

    changed date format

--- components/blogs.vue
+++ components/blogs.vue
@@ -5,16 +5,16 @@
         </div>
         <div class="flex justify-between">
             <h4 class="text-base font-medium underline underline-offset-4 decoration-1">the race for building ai</h4>
-            <p class="text-sm text-gray-300/75">aug 31st, 2023</p>
+            <p class="text-sm text-gray-300/75">aug 31, 2023</p>
         </div>
         <div class="flex justify-between">
             <h4 class="text-base font-medium underline underline-offset-4 decoration-1">why oss is important?</h4>
-            <p class="text-sm text-gray-300/75">aug 15th, 2023</p>
+            <p class="text-sm text-gray-300/75">aug 15, 2023</p>
         </div>
         <div class="flex justify-between">
             <h4 class="text-base font-medium underline underline-offset-4 decoration-1">how chatgpt has improved over the
                 months</h4>
-            <p class="text-sm text-gray-300/75">aug 01st, 2023</p>
+            <p class="text-sm text-gray-300/75">aug 01, 2023</p>
         </div>
         <div>
             <nuxtlink class="text-sm text-gray-300/75 underline underline-offset-4">
