da15a6ce8104c7cbc7de373b91660a80d06f4c2d
ajaykrupalk <ajaykrupalk@outlook.in>
2023-09-19 21:34:49 +0530

    Changed favicon and hover

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col gap-6 mx-auto p-10 md:w-[40em]">
-        <NuxtLink to="/blog" class="text-sm text-gray-300/75 underline underline-offset-4">&#8592; back</NuxtLink>
+        <NuxtLink to="/blog" class="text-sm text-gray-300/75 underline underline-offset-4 hover:text-gray-300">&#8592; back</NuxtLink>
         <h3 class="text-xl font-medium">{{ data.title }}</h3>
         <div v-html="data.content" class="space-y-5 text-gray-300/95"></div>
     </div>
@@ -16,7 +16,7 @@ const { data } = await useFetch(`${config.public.BACKEND_URI}/blog/${slug}`);
 </script>
 
 <style>
-li{
+li {
     margin-top: 1.25rem;
 }
 </style>
