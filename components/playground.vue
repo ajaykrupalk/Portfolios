1f4ccb8635c8b5dd7bfb7e1f56d7a4edb0de2390
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2024-02-20 20:44:44 +0530

    added chat with pdf project

--- components/playground.vue
+++ components/playground.vue
@@ -4,6 +4,12 @@
             <h3 class="font-mono text-xl">experiments</h3>
         </div>
         <div>
+            <a href="https://chat-in-js.vercel.app/" class="space-y-0.5" target="_blank">
+                <h4 class="text-base font-medium underline underline-offset-4 decoration-1">@ai/chat</h4>
+                <p class="text-sm text-gray-300/75">using gemini pro chat with your pdf</p>
+            </a>
+        </div>
+        <div>
             <a href="https://aitranscribe.vercel.app/" class="space-y-0.5" target="_blank">
                 <h4 class="text-base font-medium underline underline-offset-4 decoration-1">@ai/transcribe</h4>
                 <p class="text-sm text-gray-300/75">ai powered transcription of audio to text.</p>
