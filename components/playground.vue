33669ccf1c5c631253d2b7f37b3fc02473a53610
ajaykrupalk <ajaykrupalk@outlook.in>
2024-07-02 23:45:11 +0530

    added hovers for links

--- components/playground.vue
+++ components/playground.vue
@@ -5,19 +5,19 @@
         </div>
         <div>
             <a href="https://chat-in-js.vercel.app/" class="space-y-0.5" target="_blank">
-                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono">chat/ai</h4>
+                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono hover:text-gray-300/75">chat/ai</h4>
                 <p class="text-sm text-gray-300/75">using gemini pro chat with your pdf</p>
             </a>
         </div>
         <div>
             <a href="https://langground.vercel.app/" class="space-y-0.5" target="_blank">
-                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono">langground/ai</h4>
+                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono hover:text-gray-300/75">langground/ai</h4>
                 <p class="text-sm text-gray-300/75">multi-model playground for langchain</p>
             </a>
         </div>
         <div>
             <a href="https://aitranscribe.vercel.app/" class="space-y-0.5" target="_blank">
-                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono">transcribe/ai</h4>
+                <h4 class="text-base font-medium underline underline-offset-4 decoration-1 font-mono hover:text-gray-300/75">transcribe/ai</h4>
                 <p class="text-sm text-gray-300/75">ai powered transcription of audio to text.</p>
             </a>
         </div>
