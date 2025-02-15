5032a9e93d3a0ecf29d2492aa223d1a8ca0eb8f5
ajaykrupalk <ajaykrupalk@outlook.in>
2025-02-15 18:31:08 +0530

    deployment-changes

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -33,11 +33,29 @@ export const HomePage = () => {
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-3">
-                    <h2 className="font-bold">Let's Connect</h2>
+                    <h2 className="font-bold">Let&apos;s Connect</h2>
                     <div className="flex flex-row gap-x-4 opacity-75">
-                        <a className="flex flex-row gap-x-1 items-center">Email</a>
-                        <a className="flex flex-row gap-x-1 items-center">GitHub</a>
-                        <a className="flex flex-row gap-x-1 items-center">Twitter</a>
+                        <a className="flex flex-row gap-x-1 items-center" href="mailto:ajaykrupalk@gmail.com">
+                            <p className="underline underline-offset-4 decoration-2 decoration-gray-300">Email</p>
+                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                                </path>
+                            </svg>
+                        </a>
+                        <a className="flex flex-row gap-x-1 items-center" href="https://github.com/ajaykrupalk">
+                            <p className="underline underline-offset-4 decoration-2 decoration-gray-300">GitHub</p>
+                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                                </path>
+                            </svg>
+                        </a>
+                        <a className="flex flex-row gap-x-1 items-center" href="https://x.com/ajaykrupalk">
+                            <p className="underline underline-offset-4 decoration-2 decoration-gray-300">Twitter</p>
+                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                                </path>
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
